44151b4f2817eb3d66f65afb8ff05ea277a6c39b
Suyono Sukorame <suyonosukorame@ymail.com>
2025-08-28 22:06:45 +0800

    Navigation and Layout Function Practice

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link";
+
 export default function LatihanPage() {
   return (
     <>
       <h1>Halaman Blog</h1>
-      <p>list of posts</p>
+      <p>List of posts</p>
+      <ul>
+        <li>
+          <Link href="/blog/judul-berita" prefetch={false}>
+            Judul Berita
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog/belajar-next" prefetch={false}>
+            Belajar Next JS
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog/latihan-route-next" prefetch={false}>
+            Latihan Route Next JS
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
 import Link from "next/link"
 
+export const metadata = {
+  title: "Blog NextJS",
+}
+
 export default function Layout({ children }) {
   return (
     <html lang="en">
-        <head>
-            <title>Blog NextJS</title>
-        </head>
-        <body>
-            <header>
-                <nav>
-                    <ul>
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/blog">Blog</Link></li>
-                        <li><Link href="/about">About</Link></li>
-                        <li><Link href="/contact">Contact</Link></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>{children}</main>
-            <footer>
-                [footer]
-            </footer>
-        </body>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><Link href="/" prefetch={false}>Home</Link></li>
+              <li><Link href="/blog" prefetch={false}>Blog</Link></li>
+              <li><Link href="/about" prefetch={false}>About</Link></li>
+              <li><Link href="/contact" prefetch={false}>Contact</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>
+            <hr />
+            <span>I'am here to stay (footer)</span>
+        </footer>
+      </body>
     </html>
   )
 }
